f15e3fb8aab45d907aa582ddfeccbea881c85c0b
CJFinnan <CJFinnan@gmail.com>
2018-03-08 13:34:36 +0000

    Add files via upload

---
+++ AMP/ViewModels/WorkflowVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using AMP.ARIESModels;
+using AMP.Models;
+
+namespace AMP.ViewModels
+{
+    public class WorkflowVM
+    {
+        public ProjectHeaderVM ProjectHeaderVm { get; set; }
+        public WorkflowMasterVM WorkflowRequest { get; set; }
+        public WorkflowMasterVM WorkflowResponse { get; set; }
+
+    
+        public string TaskDescription { get; set; }
+        public int TaskID { get; set; }
+        public string UserRole { get; set; }
+        public string CurrentAction { get; set; }
+        public bool RequiresDelegatedAuthorityApproval { get; set; }
+        public string DocumentID { get; set; }
+        public string DocumentDescription { get; set; }
+        public string DocSource { get; set; }
+        public string DocumentLink { get; set; }
+        public bool IsDocumentRequired { get; set; }
+        public string WfMessage { get; set; }
+        public decimal BudgetValueToBeApproved { get; set; }
+       // public BudgetApprovalValue budgetApprovedByWorkflow { get; set; }
+    }
+}
